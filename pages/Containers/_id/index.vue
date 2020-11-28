a6b612a89d21e467a4e53500c1bac254805a6e56
Tudor <t.petrariu@jobsrepublic.nl>
2020-11-28 16:08:58 +0100

    styled product details page and added separated scss file for media queries, reused addToCart Butoon as well

--- pages/Containers/_id/index.vue
+++ pages/Containers/_id/index.vue
@@ -1,31 +1,67 @@
 <template>
-  <div>
-    <div v-for="item in currentProduct.sizes" class="details-card-item">
+  <b-container
+    id="card_details_page"
+    class="d-flex justify-content-center align-items-center"
+  >
+    <div class="details-card-item d-flex align-items-center">
       <div class="details-card-img">
-        <img :src="item.image" alt="" />
+        <img :src="filterSelectedSize[0].image" alt="" />
       </div>
       <div class="details-card-info">
         <div class="details-card-date">
           <span>Sunday</span>
           <span>October 21</span>
         </div>
-        <h1 class="details-card-title"></h1>
+        <h1 class="details-card-title">Container name</h1>
         <p class="details-card-text">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
           animi nisi accusamus. Quibusdam sapiente quos non veritatis totam quo
           et, ab porro labore! Earum, libero blanditiis. Fuga facilis cum odio?
         </p>
-        <a class="details-card-actions" href="#"></a>
+        <AddToCartButton
+          :item="currentProduct"
+          :price="filterSelectedSize[0].unit_price_pickup"
+        />
+        <b-form-select class="w-50 details-card-actions" v-model="selectedSize">
+          <template #first>
+            <b-form-select-option :value="selectedSize" disabled>{{
+              selectedSize
+            }}</b-form-select-option>
+          </template>
+          <b-form-select-option
+            v-for="size in currentProduct.sizes"
+            :value="size.size"
+            :key="size.size"
+            >{{ size.size }} L</b-form-select-option
+          >
+        </b-form-select>
       </div>
     </div>
-  </div>
+  </b-container>
 </template>
 
 <script>
+import AddToCartButton from '../../../components/common/AddToCartButton'
 export default {
+  components: {
+    AddToCartButton
+  },
+  data() {
+    return {
+      selectedSize: 'Size'
+    }
+  },
   computed: {
     currentProduct() {
       return this.$store.getters['containers/getCurrentProduct']
+    },
+    filterSelectedSize() {
+      if (this.selectedSize === 'Size') {
+        return this.currentProduct.sizes
+      }
+      return this.currentProduct.sizes.filter((product) => {
+        return product.size === this.selectedSize
+      })
     }
   }
 }
